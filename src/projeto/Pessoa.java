ad1792da054a8e5bd5cd8aa494602c433d1201f2
Yuri Cifuentes <yuria.cifuentes@gmail.com>
2022-06-25 17:41:03 -0300

    Initial commit

---
+++ src/projeto/Pessoa.java
@@ -0,0 +1,32 @@
+package projeto;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+	private final String nome;
+	private final LocalDate birthday;
+	private String redeSocial;
+	private String email;
+	private boolean emprego;
+
+	public Pessoa(String nome, LocalDate nascimento) {
+		this.nome = nome;
+		this.birthday = nascimento;
+	}
+
+	public Pessoa(String nome, LocalDate nascimento, String email, String redeSocial) {
+		this.nome = nome;
+		this.birthday = nascimento;
+		this.redeSocial = redeSocial;
+		this.email = email;
+	}
+
+	public Pessoa(String nome, LocalDate nascimento, String email, boolean emprego, String redeSocial) {
+		this.nome = nome;
+		this.birthday = nascimento;
+		this.redeSocial = redeSocial;
+		this.email = email;
+		this.emprego = emprego;
+	}
+
+}
