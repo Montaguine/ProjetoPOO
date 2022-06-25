13e67cae08d4d71662b6c42093abb6089809297f
Yuri Cifuentes <yuria.cifuentes@gmail.com>
2022-06-25 20:54:21 -0300

    Adiciona Getters/setter e exception
    Getters e setters adicionados;
    
    Exception lançada nos construtores para não permitir data maior que a atual;

--- src/projeto/Pessoa.java
+++ src/projeto/Pessoa.java
@@ -10,17 +10,24 @@ public class Pessoa {
 	private boolean trabalha;
 
 	public Pessoa(String nome, LocalDate nascimento) {
+		if (nascimento.getYear() > LocalDate.now().getYear())
+			throw new IllegalArgumentException("Data inválida");
 		this.nome = nome;
 		this.birthday = nascimento;
 	}
 
 	public Pessoa(String nome, LocalDate nascimento, String email, String redeSocial) {
+		if (nascimento.getYear() > LocalDate.now().getYear())
+			throw new IllegalArgumentException("Data inválida");
 		this.nome = nome;
 		this.birthday = nascimento;
 		this.redeSocial = redeSocial;
 		this.email = email;
 	}
+
 	public Pessoa(String nome, LocalDate nascimento, String email, boolean emprego, String redeSocial) {
+		if (nascimento.getYear() > LocalDate.now().getYear())
+			throw new IllegalArgumentException("Data inválida");
 		this.nome = nome;
 		this.birthday = nascimento;
 		this.redeSocial = redeSocial;
@@ -28,4 +35,41 @@ public class Pessoa {
 		this.trabalha = emprego;
 	}
 
+	public String getNome() {
+		return this.nome;
+	}
+
+	public int getIdade() {
+		return this.birthday.getYear();
+	}
+
+	public String getSocial() {
+		return this.redeSocial;
+	}
+
+	public void setSocial(String redeSocial) {
+		this.redeSocial = redeSocial;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean seTrabalha() {
+		return this.trabalha;
+	}
+
+	public void setTrabalha(boolean trabalha) {
+		this.trabalha = trabalha;
+	}
+
+	@Override
+	public String toString() {
+		return this.getNome() + ";" + this.getIdade() + ";" + this.getEmail() + ";" + this.getSocial() + ";"
+				+ this.seTrabalha();
+	}
 }
