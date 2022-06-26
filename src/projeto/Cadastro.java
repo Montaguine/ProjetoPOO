8ead7117424fcb5e266fca550405eb4b8fdf71ee
Yuri Cifuentes <yuria.cifuentes@gmail.com>
2022-06-25 21:26:30 -0300

    Incluídas classes Cadastro e testaCadastro
    Classes cadastro e testaCadastro adicionadas para realização de teste inicial;
    
    Classe Cadastro inclui um menu com opções;
    
    Classe testaCadastro executa e testa menu;

---
+++ src/projeto/Cadastro.java
@@ -0,0 +1,32 @@
+package projeto;
+
+import java.util.Scanner;
+
+public class Cadastro {
+
+	public static final Scanner INP = new Scanner(System.in);
+
+	public static int opcao() {
+		int opcao = INP.nextInt();
+		if (opcao > 4 || opcao < 0)
+			throw new IllegalArgumentException("Digite uma opção válida");
+		return opcao;
+	}
+
+	public static void menu() {
+		System.out.println("Cadastro de " + Pessoa.class.getSimpleName()+"\n\n");
+		System.out.println("(0)sair");
+		System.out.println("(1)inserir " + Pessoa.class.getSimpleName());
+		System.out.println("(2)listar  " + Pessoa.class.getSimpleName());
+		System.out.println("(3)buscar  " + Pessoa.class.getSimpleName());
+		System.out.println("(4)deletar " + Pessoa.class.getSimpleName());
+
+		try {
+			int menu = 0;
+			menu = opcao();
+		} catch (IllegalArgumentException e) {
+			System.out.println("Opção inválida, tente novamente\n\n");
+			menu();
+		}
+	}
+}
---
+++ src/projeto/testaCadastro.java
@@ -0,0 +1,8 @@
+package projeto;
+
+public class testaCadastro extends Cadastro {
+
+	public static void main(String[] args) {
+		menu();
+	}
+}
